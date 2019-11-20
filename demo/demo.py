76aa76966fcf43efc9cd24e72fa66315d2283cfe
cswu <xray0h@gmail.com>
2019-11-20 22:50:27 +0800

    run analysis

--- demo/demo.py
+++ demo/demo.py
@@ -1,1 +1,54 @@
-from vocoder_eva.eval import eval_MCD, rmse_f0, eval_snr
+from vocoder_eva.eval import eval_MCD, eval_rmse_f0, eval_snr
+import os
+from glob import glob
+from pathlib import Path
+import librosa
+
+
+class EvaDataset:
+
+    def __init__(self, raw_folder, syn_folder):
+        self.r_files = {Path(y).stem: Path(y) for x in os.walk(raw_folder) for y in glob(os.path.join(x[0], '*.wav'))}
+        self.s_files = {Path(y).stem: Path(y) for x in os.walk(syn_folder) for y in glob(os.path.join(x[0], '*.wav'))}
+
+        assert len(self.r_files) == len(self.r_files)
+
+        self.file_names = set(self.r_files.keys()).union(set(self.s_files.keys()))
+        self.file_names = list(self.file_names)
+
+    def __len__(self):
+        return len(self.file_names)
+
+    def __getitem__(self, idx):
+        file_name = self.file_names[idx]
+
+        r_file = self.r_files[file_name]
+        s_file = self.s_files[file_name]
+
+        aud_r, sr_r = librosa.load(r_file, sr=None)
+        aud_s, sr_s = librosa.load(s_file, sr=None)
+
+        assert sr_r == sr_s
+
+        return aud_r, aud_s, sr_r
+
+
+if __name__ == '__main__':
+    r_folder = '../data/ground_truth'
+    s_folder = '../data/repeat1_no_pulse'
+
+    d = EvaDataset(r_folder, s_folder)
+
+    f0_rmse_list = []
+    vuv_precision_list = []
+
+    for aud_r, aud_s, sr in d:
+        f0_rmse_mean, vuv_precision = eval_rmse_f0(aud_r, aud_s, sr)
+        print(f0_rmse_mean, vuv_precision)
+        f0_rmse_list.append(f0_rmse_mean)
+        vuv_precision_list.append(vuv_precision)
+
+    avg_f0_rmse = sum(f0_rmse_list) / len(d)
+    avg_vuv_precision = sum(vuv_precision_list) / len(d)
+
+    print('avg_f0_rmse:', avg_f0_rmse, 'avg_vuv_precision:', avg_vuv_precision)
--- vocoder_eva/eval.py
+++ vocoder_eva/eval.py
@@ -4,18 +4,16 @@ import pyworld as pw
 import matplotlib.pyplot as plt
 import pysptk
 
-file_r = 'vocoder_eva/arctic_a0001_hpf.wav'
-file_s = 'vocoder_eva/arctic_a0001_nwf.wav'
-
-aud_r, sr_r = librosa.load(file_r, sr=None)
-aud_s, sr_s = librosa.load(file_s, sr=None)
-
-# print(aud_r.shape, aud_s.shape)
-
-assert sr_r == sr_s
-assert len(aud_r) == len(aud_s)
-
 ln10_inv = 1 / np.log(10)
+
+
+def pad_to(x, target_len):
+    pad_len = target_len - len(x)
+
+    if pad_len <= 0:
+        return x[:target_len]
+    else:
+        return np.pad(x, (0, pad_len), 'constant', constant_values=(0, 0))
 
 
 def eval_snr(x_r, x_s):
@@ -47,8 +45,8 @@ def eval_rmse_f0(x_r, x_s, sr, frame_len='5', method='swipe'):
         f0_r, t = pw.harvest(x_r.astype(np.double), sr, frame_period=100)
         f0_s, t = pw.harvest(x_s.astype(np.double), sr, frame_period=100)
     elif method == 'dio':
-        f0_r, t = pw.harvest(x_r.astype(np.double), sr, frame_period=100)
-        f0_s, t = pw.harvest(x_s.astype(np.double), sr, frame_period=100)
+        f0_r, t = pw.dio(x_r.astype(np.double), sr, frame_period=100)
+        f0_s, t = pw.dio(x_s.astype(np.double), sr, frame_period=100)
     elif method == 'swipe':
         f0_r = pysptk.sptk.swipe(x_r.astype(np.double), sr, hopsize=128)
         f0_s = pysptk.sptk.swipe(x_s.astype(np.double), sr, hopsize=128)
@@ -59,15 +57,16 @@ def eval_rmse_f0(x_r, x_s, sr, frame_len='5', method='swipe'):
         raise ValueError('no such f0 exract method')
     # TODO: frame_len
     # print(f0_r, f0_s)
+    f0_s = pad_to(f0_s, len(f0_r))
     f0_r_unv = (f0_r == 0) * 1
     f0_s_unv = (f0_s == 0) * 1
 
-    voiced_mask = (1-f0_r_unv) * (1-f0_s_unv)
+    voiced_mask = (1 - f0_r_unv) * (1 - f0_s_unv)
 
     print(f0_r.shape, f0_s.shape)
 
-    y = 1200 * np.abs(np.log2(f0_r+f0_r_unv) - np.log2(f0_s+f0_s_unv))
-    y = y * (1-f0_r_unv) * (1-f0_s_unv)
+    y = 1200 * np.abs(np.log2(f0_r + f0_r_unv) - np.log2(f0_s + f0_s_unv))
+    y = y * (1 - f0_r_unv) * (1 - f0_s_unv)
     # y = np.nan_to_num(y)
 
     f0_rmse_mean = (y * voiced_mask).sum() / voiced_mask.sum()
@@ -77,6 +76,16 @@ def eval_rmse_f0(x_r, x_s, sr, frame_len='5', method='swipe'):
 
 
 if __name__ == '__main__':
+    file_r = 'vocoder_eva/arctic_a0001_hpf.wav'
+    file_s = 'vocoder_eva/arctic_a0001_nwf.wav'
+
+    aud_r, sr_r = librosa.load(file_r, sr=None)
+    aud_s, sr_s = librosa.load(file_s, sr=None)
+
+    # print(aud_r.shape, aud_s.shape)
+
+    assert sr_r == sr_s
+    assert len(aud_r) == len(aud_s)
     # mcd = eval_MCD(aud_r, aud_s)
     rmse_f0 = eval_rmse_f0(aud_r, aud_s, sr_r)
     print(rmse_f0)
