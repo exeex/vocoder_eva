add todo, fix vuv accuracy, precision

vocoder_eva/eval.py
@@ -17,11 +17,12 @@ def pad_to(x, target_len):
 
 
 def eval_snr(x_r, x_s):
-    # TODO: slide x_s to find max matched value
+    # TODO: slide x_s to find max matched value 原論文有做滑動x_s，找到最大匹配的snr值，這邊還沒實作
     return 10 * np.log10(np.sum(x_s ** 2) / np.sum((x_s - x_r) ** 2))
 
 
 def eval_MCD(x_r, x_s):
+    # TODO: verify value 確認做出來的值是否正確 (和原論文比較)
     c_r = librosa.feature.mfcc(x_r)
     c_s = librosa.feature.mfcc(x_s)
 
@@ -41,6 +42,7 @@ def eval_MCD(x_r, x_s):
 
 
 def eval_rmse_f0(x_r, x_s, sr, frame_len='5', method='swipe'):
+    # TODO: 要可以改動 frame len (ms) 或者 hop_size
     if method == 'harvest':
         f0_r, t = pw.harvest(x_r.astype(np.double), sr, frame_period=50)
         f0_s, t = pw.harvest(x_s.astype(np.double), sr, frame_period=50)
@@ -55,37 +57,51 @@ def eval_rmse_f0(x_r, x_s, sr, frame_len='5', method='swipe'):
         f0_s = pysptk.sptk.rapt(x_s.astype(np.double), sr, hopsize=128)
     else:
         raise ValueError('no such f0 exract method')
-    # TODO: frame_len
-    # print(f0_r, f0_s)
+
+    # length align
     f0_s = pad_to(f0_s, len(f0_r))
-    f0_r_unv = (f0_r == 0) * 1
-    f0_s_unv = (f0_s == 0) * 1
 
-    voiced_mask = (1 - f0_r_unv) * (1 - f0_s_unv)
+    # make unvoice / vooiced frame mask
+    f0_r_uv = (f0_r == 0) * 1
+    f0_r_v = 1 - f0_r_uv
+    f0_s_uv = (f0_s == 0) * 1
+    f0_s_v = 1 - f0_s_uv
 
-    # print(f0_r.shape, f0_s.shape)
+    tp_mask = f0_r_v * f0_s_v
+    tn_mask = f0_r_uv * f0_s_uv
+    fp_mask = f0_r_uv * f0_s_v
+    fn_mask = f0_r_v * f0_s_uv
 
-    y = 1200 * np.abs(np.log2(f0_r + f0_r_unv) - np.log2(f0_s + f0_s_unv))
-    y = y * (1 - f0_r_unv) * (1 - f0_s_unv)
-    # y = np.nan_to_num(y)
+    # only calculate f0 error for voiced frame
+    y = 1200 * np.abs(np.log2(f0_r + f0_r_uv) - np.log2(f0_s + f0_s_uv))
+    y = y * tp_mask
+    f0_rmse_mean = y.sum() / tp_mask.sum()
 
-    f0_rmse_mean = (y * voiced_mask).sum() / voiced_mask.sum()
-    precision = voiced_mask.sum() / (1 - f0_r_unv).sum()
+    # only voiced/ unvoiced accuracy/precision
+    vuv_precision = tp_mask.sum() / (tp_mask.sum() + fp_mask.sum())
+    vuv_accuracy = (tp_mask.sum() + tn_mask.sum()) / len(y)
 
-    return f0_rmse_mean, precision
+    return f0_rmse_mean, vuv_accuracy, vuv_precision
+
+
+def eval_rmse_ap(x_r, x_s, sr, frame_len='5'):
+    # TODO: find out what algorithm to use.  maybe pyworld d4c?
+    pass
 
 
 if __name__ == '__main__':
-    file_r = 'vocoder_eva/arctic_a0001_hpf.wav'
-    file_s = 'vocoder_eva/arctic_a0001_nwf.wav'
+
+    file_r = 'demo/exmaple_data/ground_truth/arctic_b0436.wav'
+    file_s = 'demo/exmaple_data/no_pulse/arctic_b0436.wav'
 
     aud_r, sr_r = librosa.load(file_r, sr=None)
     aud_s, sr_s = librosa.load(file_s, sr=None)
 
-    # print(aud_r.shape, aud_s.shape)
-
     assert sr_r == sr_s
-    assert len(aud_r) == len(aud_s)
+    if len(aud_r) != len(aud_s):
+        aud_r = aud_r[:len(aud_s)]
+        aud_s = aud_s[:len(aud_r)]
+
     # mcd = eval_MCD(aud_r, aud_s)
     rmse_f0 = eval_rmse_f0(aud_r, aud_s, sr_r)
     print(rmse_f0)